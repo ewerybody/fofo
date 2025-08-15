6f626f1811c191701f652f39797a13f67a04f0be
Eric Werner <ewerybody+github@gmail.com>
2025-08-15 15:05:04 +0200

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,15 @@
+function menu_toggle() {
+    var btn = document.getElementsByClassName("overlay-btn")[0];
+    btn.classList.toggle("overlay-btn-x");
+    var menu = document.getElementsByClassName("menu-overlay")[0];
+    menu.classList.toggle("menu-overlay-x");
+}
+
+function _connect_nb_btn() {
+    const btn = document.getElementsByClassName("overlay-btn")[0];
+    btn.addEventListener('click', event => {
+        menu_toggle();
+    });
+}
+
+window.onload=_connect_nb_btn;
